737b3f5e0cc9dc62a54fa141c9972f45041d75e8
Ahmed Soliman <ahmadsoliman283@gmail.com>
2026-01-27 10:36:30 +0200

    Starting create My Portfolio

---
+++ constant/constant.ts
@@ -0,0 +1,22 @@
+export const navlinks = [
+  {
+    id: 1,
+    url: "#about",
+    label: "About",
+  },
+  {
+    id: 2,
+    url: "#projects",
+    label: "Projects",
+  },
+  {
+    id: 3,
+    url: "#experience",
+    label: "Experience",
+  },
+  {
+    id: 4,
+    url: "#testimonials",
+    label: "Testimonials",
+  },
+];
